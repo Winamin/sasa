c3ab18468278fd6f0a3e4bddbfac54e5cb6e7915
Link <yingxuw814@gmail.com>
2025-01-01 11:08:38 +0800

    6ch

--- src/renderer.rs
+++ src/renderer.rs
@@ -8,4 +8,5 @@ pub trait Renderer: Send + Sync {
     fn alive(&self) -> bool;
     fn render_mono(&mut self, sample_rate: u32, data: &mut [f32]);
     fn render_stereo(&mut self, sample_rate: u32, data: &mut [f32]);
+    fn render_6ch(&mut self, sample_rate: u32, data: &mut [f32]);
 }
